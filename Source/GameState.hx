4ab28c673c78c78b5294ee68f3ef319afea8eb0e
MintPaw <jerusanders@gmail.com>
2014-11-08 12:25:35 -0700

    Initial commit
    Set up project

---
+++ Source/GameState.hx
@@ -0,0 +1,18 @@
+package ;
+
+import flixel.FlxG;
+import flixel.FlxState;
+
+class GameState extends FlxState
+{
+
+	public function new()
+	{
+		super();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
---
+++ Source/Main.hx
@@ -0,0 +1,19 @@
+package;
+
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+
+
+class Main extends Sprite
+{
+		
+	public function new()
+	{
+		var flixel:FlxGame = new FlxGame(640, 360, GameState, 1, 60, 60, true);
+		addChild(flixel);
+
+		super();	
+	}
+
+}
